cc1eed779dde05ca0d5b344fd03f2a61a0d779ff
Danilo Castro <danilogcastro@hotmail.com>
2021-05-25 15:12:02 -0300

    Created and migrated db, seeds and created controller

--- Gemfile
+++ Gemfile
@@ -49,3 +49,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Write simpler forms
+gem 'simple_form'
+
+# Generate fake content
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 10 fake articles'
+
+10.times do
+  article = Article.new(
+    title: "#{Faker::GreekPhilosophers.name}'s #{Faker::Marketing.buzzwords}",
+    content: Faker::GreekPhilosophers.quote
+  )
+  article.save
+end
+
+puts 'All done!'
